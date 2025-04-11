75b4f4ac9908e7495361a59f78988428cfe06bf8
AmeyaVelankarUW <velankar@uw.edu>
2025-04-10 17:12:56 -0700

    build v1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5' 
+VERSION = '1.0.0' 
 DESCRIPTION = 'LumericPy: A Python package for Lumerical FDTD and MODE Solutions'
 LONG_DESCRIPTION = 'LumericPy is a Python package designed to facilitate the use of Lumerical FDTD and MODE Solutions software. It provides a set of tools and functions to streamline the process of simulating and analyzing photonic devices using these powerful simulation tools.'
 
